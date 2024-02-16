655d1f925d88d7e384d83e5985bbe69d894a26e2
Aqsagull99 <159878357+Aqsagull99@users.noreply.github.com>
2024-02-17 00:57:19 +0500

    Add files via upload

---
+++ QNO15.ts
@@ -0,0 +1,22 @@
+// //  QNO:15
+
+// // Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+// // • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+// // • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+// // • Print a second set of invitation messages, one for each person who is still in your list.
+
+// // More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+
+// // let guestlist=["saba","noreen","naila","musfira","mehru", "muntaha"]
+// //  let canNotAttend:string = "noreen"
+// //  console.log(canNotAttend + '' + "can not make it for dinner ");
+
+//  let newGuest:string ="mahru"
+//  guestlist[guestlist.indexOf(canNotAttend)] =newGuest;
+
+
+// guestlist.map((item)=>
+// console.log(`dear ${item}, you are cordinally invited to dinner`))
